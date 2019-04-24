3e0b02bce10fc66f8284b78a99261312a507b719
AlexCh98 <alexcheca.98@gmail.com>
2019-04-24 14:16:56 +0200

    Cambios BackTracking Alex

--- src/Alex/EjerciciosProblemasClase/ClasePracticaVoracesGrafos_BackTracking/Carreteras.java
+++ src/Alex/EjerciciosProblemasClase/ClasePracticaVoracesGrafos_BackTracking/Carreteras.java
@@ -5,6 +5,9 @@ package Alex.EjerciciosProblemasClase.ClasePracticaVoracesGrafos_BackTracking;
 import java.util.*;
 
 public class Carreteras {
+    private static int islas;
+    private static long carreteras;
+    private static long coste;
     public static class Carretera implements Comparable{
         int origen;
         int destino;
@@ -21,60 +24,70 @@ public class Carreteras {
         public int compareTo(Object o) {
             Carretera otra = (Carretera) o;
             if (this.coste == otra.coste) {
-                if (this.origen == otra.origen && this.destino == otra.destino
-                        || this.destino == otra.origen && this.origen == otra.destino) {
+                if (this.origen == otra.origen && this.destino == otra.destino || this.destino == otra.origen && this.origen == otra.destino) {
                     return 0;
                 } else {
                     return -1;
                 }
             } else {
-                return Integer.compare(otra.coste, this.coste);
+                return Integer.compare(otra.coste, this.coste); //Maximizar
+                //return Integer.compare(this.coste, otra.coste); //Minimizar
             }
         }
+
     }
     public static void main(String [] args){
         Scanner lector = new Scanner(System.in);
         int numeroCiudades = Integer.parseInt(lector.nextLine());
         while(numeroCiudades != 0){
             int numeroCarreteras = Integer.parseInt(lector.nextLine());
-            //ArrayList<Carretera> listadoCarreteras = new ArrayList<>();
-            int[][] listadoCarreteras = new int[numeroCiudades][numeroCiudades];
+            List[] listadoCarreteras = new List[numeroCiudades];
+            for(int i = 0; i < numeroCiudades;i++){
+                listadoCarreteras[i] = new ArrayList();
+            }
             for (int j = 0; j < numeroCarreteras;j++){
                 String[] linea = lector.nextLine().split(" ");
                 int origen = Integer.parseInt(linea[0]);
                 int destino = Integer.parseInt(linea[1]);
                 int peso = Integer.parseInt(linea[2]);
-                listadoCarreteras[origen][destino] = Math.max(listadoCarreteras[origen][destino],peso);
-                listadoCarreteras[destino][origen] = Math.max(listadoCarreteras[destino][origen],peso);
+                listadoCarreteras[origen].add(new Carretera(origen,destino,peso));
+                listadoCarreteras[destino].add(new Carretera(destino,origen,peso));
             }
-
             Prim(listadoCarreteras);
+            System.out.println(islas+" "+carreteras+" "+coste);
+            System.out.println("---");
+            coste = 0;
+            islas = 0;
+            carreteras = 0;
             numeroCiudades = Integer.parseInt(lector.nextLine());
 
         }
     }
 
-    private static List<Carretera> Prim(int[][] matrizAdaycencia) {
-        List<Carretera> solucion = new ArrayList<>();
-        int n = matrizAdaycencia.length;
+    private static List<Carretera> Prim(List[] listadoCarreteras) {
+        boolean[] visitado = new boolean[listadoCarreteras.length];
+        List<Carretera> isla = new ArrayList<>();
 
-        int verticeOrigen = (int) (Math.random() * (n - 1)) + 1;
-        System.out.println("NODO ORIGEN:"+verticeOrigen);
-        HashSet<Integer> vertices = new HashSet<>();
-        PriorityQueue<Carretera> cola = new PriorityQueue<>();
-
-        vertices.add(verticeOrigen);
-        while (vertices.size() < n - 1) {
-            rellenarCola(cola, matrizAdaycencia, verticeOrigen, vertices);
-            Carretera mejorArista = cola.poll();
-            assert mejorArista != null;
-            if (!vertices.contains(mejorArista.destino)) {
-                solucion.add(mejorArista);
-                verticeOrigen = mejorArista.destino;
-                vertices.add(verticeOrigen);
+        for(int i = 0; i < listadoCarreteras.length;i++){
+            if(!visitado[i]){
+                islas++;
+                PriorityQueue<Carretera> cola = new PriorityQueue<>();
+                cola.addAll(listadoCarreteras[i]);
+                visitado[i] = true;
+                while (!cola.isEmpty()){
+                    Carretera carret = cola.poll(); //Cogemos el mejor elemento y lo borramos
+                    if(!visitado[carret.destino]){
+                        coste += carret.coste;
+                        carreteras += 1;
+                        isla.add(carret);
+                        cola.addAll(listadoCarreteras[carret.destino]);
+                        visitado[carret.destino] = true;
+                    }
+                }
             }
         }
-        return solucion;
+        return isla;
+
     }
 
     private static void rellenarCola(PriorityQueue<Carretera> cola, int[][] matrizAdyacencia, int verticeOrigen, HashSet<Integer> vertices) {
