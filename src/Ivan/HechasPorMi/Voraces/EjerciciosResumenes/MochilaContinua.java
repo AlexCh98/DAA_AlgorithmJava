d6a5a492ddce6586bcb1c4f66d223deafa7ce234
i100van <i100van@hotmail.com>
2019-05-07 19:17:49 +0200

    Ejercicios

--- src/Ivan/HechasPorMi/Voraces/EjerciciosResumenes/MochilaContinua.java
+++ src/Ivan/HechasPorMi/Voraces/EjerciciosResumenes/MochilaContinua.java
@@ -1,4 +1,68 @@
 package Ivan.HechasPorMi.Voraces.EjerciciosResumenes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MochilaContinua {
+    public static class Objeto implements Comparable<Objeto>{
+        String nombre;
+        double peso;
+        double utilidad;
+        double ratio;
+
+        public Objeto(String nombre,double peso,double utilidad) {
+            this.nombre = nombre;
+            this.peso =peso;
+            this.utilidad=utilidad;
+            ratio = (double) Math.round(((this.utilidad/this.peso) * 100d));
+        }
+
+        @Override
+        public int compareTo(Objeto o) {
+            return Double.compare(this.ratio,o.ratio);
+        }
+    }
+
+    public static void main(String[] args) {
+        Objeto o1 = new Objeto("raton",1,1);
+        Objeto o2 = new Objeto("teclado",2,1);
+        Objeto o3 = new Objeto("ipad",2,3);
+        Objeto o4 = new Objeto("portatil",3,5);
+        Objeto o5 = new Objeto("mesa",15,6);
+        Objeto o6 = new Objeto("planta",4,1);
+        Objeto o7 = new Objeto("cargador",1,3);
+        Objeto o8 = new Objeto("papeles",5,5);
+        Objeto o9 = new Objeto("altavoz",8,9);
+        List<Objeto> objetos = new ArrayList<>();
+        objetos.add(o1);objetos.add(o2);objetos.add(o3);objetos.add(o4);
+        objetos.add(o5);objetos.add(o6);objetos.add(o7);objetos.add(o8);
+        objetos.add(o9);
+        int pesoMax = 22;
+        Collections.sort(objetos);
+        for (int i = 0; i <objetos.size() ; i++) {
+            System.out.println(objetos.get(i).ratio);
+        }
+        List<Objeto> solucion = new ArrayList<>();
+        greedyMochilaContinua(objetos,pesoMax,solucion);
+        Collections.sort(solucion);
+        for (int i = 0; i < solucion.size(); i++) {
+            System.out.println(solucion.get(i).nombre+" "+solucion.get(i).peso);
+            System.out.println("Ratio del objeto:"+solucion.get(i).ratio);
+        }
+    }
+
+    private static void greedyMochilaContinua(List<Objeto> objetos, int pesoMax, List<Objeto> solucion) {
+        List<Objeto> copiaObjs = objetos;
+        double pesoAux=0;
+        while ((pesoAux<pesoMax)&&(!copiaObjs.isEmpty())){
+            for (int i = 0; i <objetos.size() ; i++) {
+                if ((objetos.get(i).peso+pesoAux)<pesoMax){
+                    solucion.add(objetos.get(i));
+                    pesoAux=pesoAux+objetos.get(i).peso;
+                }copiaObjs.remove(i);
+            }
+        }
+    }
+
 }
--- src/Ivan/HechasPorMi/Voraces/EjerciciosResumenes/MochilaDiscreta.java
+++ src/Ivan/HechasPorMi/Voraces/EjerciciosResumenes/MochilaDiscreta.java
@@ -1,4 +1,76 @@
 package Ivan.HechasPorMi.Voraces.EjerciciosResumenes;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MochilaDiscreta {
+    public static class Objeto implements Comparable<Objeto>{
+        String nombre;
+        int peso;
+
+        public Objeto(String nombre,int peso) {
+            this.nombre = nombre;
+            this.peso =peso;
+        }
+
+        @Override
+        public int compareTo(Objeto o) {
+            return Integer.compare(this.peso,o.peso);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Objeto o1 = new Objeto("raton",1);
+        Objeto o2 = new Objeto("teclado",2);
+        Objeto o3 = new Objeto("ipad",2);
+        Objeto o4 = new Objeto("portatil",3);
+        Objeto o5 = new Objeto("mesa",15);
+        Objeto o6 = new Objeto("planta",4);
+        Objeto o7 = new Objeto("cargador",1);
+        Objeto o8 = new Objeto("papeles",5);
+        Objeto o9 = new Objeto("altavoz",8);
+        List<Objeto> objetos = new ArrayList<>();
+        objetos.add(o1);objetos.add(o2);objetos.add(o3);objetos.add(o4);
+        objetos.add(o5);objetos.add(o6);objetos.add(o7);objetos.add(o8);
+        objetos.add(o9);
+        int pesoMax = 22;
+        //Comprobamos que los datos estan bien introducidos
+        for (int i = 0; i < objetos.size(); i++) {
+            System.out.println(objetos.get(i).nombre+" "+objetos.get(i).peso);
+        }
+        //Ordenamos los objetos
+        Collections.sort(objetos);
+        ArrayList<Objeto>mochila=new ArrayList<>();
+        greedyMochilaDiscreta(objetos,pesoMax,mochila);
+        //Imprimimos el contenido de la mochila
+        System.out.println("Estos son los objetos cogidos");
+        for (int i = 0; i < mochila.size(); i++) {
+            System.out.println(mochila.get(i).nombre+" "+mochila.get(i).peso);
+        }
+    }
+//Vamos a maximizar el numero de objetos, entonces nuestro criterio es meter el que menos pese
+    private static void greedyMochilaDiscreta(List<Objeto> objetos, int pesoMax, ArrayList<Objeto> mochila) {
+        List<Objeto> copiaObjs = objetos;
+        int auxPeso=0;
+        while ((auxPeso<pesoMax)&&(!copiaObjs.isEmpty())){
+            for (int i = 0; i <objetos.size() ; i++) {
+                if(auxPeso+copiaObjs.get(i).peso<=pesoMax){
+                    System.out.println(" Añado Obj = " + copiaObjs.get(i).nombre);
+                    mochila.add(copiaObjs.get(i));
+                    auxPeso=auxPeso+copiaObjs.get(i).peso;
+                    copiaObjs.remove(i);
+                }
+                System.out.println(" Elimino Obj = " + copiaObjs.get(i).nombre);
+                copiaObjs.remove(i);
+            }
+
+
+
+        }
+    }
+
+
 }
